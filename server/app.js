20d74e8e4abc76be5295e4f632b7955f132d2307
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-02-28 11:31:12 +0530

    chore: make server active

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import jwt from "jsonwebtoken";
+import passport from "passport";
+import session from "express-session";
+import { Strategy as LocalStrategy } from "passport-local";
+import path from "node:path";
+import url from "node:url";
+
+// imports for server side routes and modules
+
+const app = express();
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, (err) => {
+  if (err) throw new Error(err);
+  console.log(`App is running on http://localhost:${PORT}`);
+});
