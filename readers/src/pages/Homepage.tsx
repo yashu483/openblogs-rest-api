80af33cc62fd41803b9b201559340f01df10b302
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-03-04 18:54:14 +0530

    feat: create and design homepage for reader app

--- readers/src/pages/Homepage.tsx
+++ readers/src/pages/Homepage.tsx
@@ -1,3 +1,32 @@
+import styles from "./styles/Homepage.module.css";
+import heroImage from "@/assets/images/hero.svg";
+import { Link } from "react-router-dom";
+
 export const Homepage = () => {
-  return <h1>This is Homepage</h1>;
+  return (
+    <div className={styles.main}>
+      <h1 className={styles.mainHeading}>
+        Share your story on <span>OpenBlogs</span>
+      </h1>
+      <div className={styles.heroWrapper}>
+        <img
+          src={heroImage}
+          alt="Hero image to display users browsing posts"
+          aria-hidden="true"
+        />
+      </div>
+      <p>
+        A modern platform for writers and readers to share ideas, stories, and
+        knowledge in a beautiful, engaging way.
+      </p>
+      <div className={styles.homepageButtons}>
+        <Link to="/posts" className={styles.postsLink}>
+          Explore Posts
+        </Link>
+        <a href="" className={styles.writerLink}>
+          Write on our platform
+        </a>
+      </div>
+    </div>
+  );
 };
